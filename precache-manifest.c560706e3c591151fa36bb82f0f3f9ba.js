284b52c65ae4a7eed65377f71cd8c872707890e0
hrithiksh101 <hrithik.sh.13@gmail.com>
2020-09-18 22:21:40 +0530

    Updates

---
+++ precache-manifest.c560706e3c591151fa36bb82f0f3f9ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391e495e26e34c237e7ae47d92cdd91b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "cc4b7c7423a6eb99a6f0",
+    "url": "/my-app/static/css/main.e660d8f9.chunk.css"
+  },
+  {
+    "revision": "38dd7ba3c49b34b91919",
+    "url": "/my-app/static/js/2.407606fb.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/my-app/static/js/2.407606fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc4b7c7423a6eb99a6f0",
+    "url": "/my-app/static/js/main.8b10da6a.chunk.js"
+  },
+  {
+    "revision": "97b1fe80f870443865fd",
+    "url": "/my-app/static/js/runtime-main.1823bc08.js"
+  },
+  {
+    "revision": "51069722611596d26e97e6079a71d6d9",
+    "url": "/my-app/static/media/10.51069722.jpg"
+  }
+]);
